1e990a95abfb468e65c347c2ab1655e354f85d39
Fareh Iqbal <fareh.legend@gmail.com>
2022-05-29 03:15:04 +0500

    learn

---
+++ EmployeeRecord.java
@@ -0,0 +1,15 @@
+// Simple record example that demonstrates that you do not need to set constructor,
+// getter and setter method. It also does not need hashcode() and toString() method.
+// record cannot extend other classes. The clas is marked final.
+
+
+
+public record EmployeeRecord (
+    Long id,
+    String firstname,
+    String lastname,
+    String email,
+    int age
+){
+    
+}
---
+++ RecordExample.java
@@ -0,0 +1,13 @@
+
+
+
+public class RecordExample {
+    public static void main(String[] args) {
+        EmployeeRecord e1 = new EmployeeRecord(9395L,"mango", "man", "mango@email.com", 99);
+    
+        System.out.println(e1.id());
+        System.out.println(e1);
+    
+
+    }
+}
